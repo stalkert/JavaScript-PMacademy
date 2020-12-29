71439be2547a95e4615a789a552773875d0e519f
Anton Bazhan <bazhan.anton@ukr.net>
2020-12-29 18:42:11 +0200

    feat: add task-string-1

---
+++ Home-work-5/String task/string-task-1.js
@@ -0,0 +1,21 @@
+"use strict";
+
+// Запросить  у пользователя две строки, найти сколько раз повторяется вторая в первой. Вывести индексы всех вхождений.
+// prompt("Enter the string");
+
+const firstString = prompt("Введите строку").toLocaleLowerCase();
+const secondString = prompt("Введите строку").toLocaleLowerCase();
+let arrayIndex = [];
+let total = 0;
+let index = 0;
+while (firstString.includes(secondString, index)) {
+  total++;
+  let indexStartRepeat = firstString.indexOf(secondString, index);
+  arrayIndex.push(indexStartRepeat);
+  index = indexStartRepeat + secondString.length;
+}
+alert(
+  total !== 0
+    ? `Количество повторений: ${total}. Идексы входов: ${arrayIndex} `
+    : " Нет совпадений"
+);
