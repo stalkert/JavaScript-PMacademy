88c00506a251d89234ea7e69d55b0b101c2ef8cf
Anton Bazhan <bazhan.anton@ukr.net>
2020-12-30 12:26:57 +0200

    feat: add task-string-2,3

--- Home-work-5/String task/task-string-2,3.js
+++ Home-work-5/String task/task-string-2,3.js
@@ -7,3 +7,9 @@ const str = "ECMAScript 2015 (6th Edition, ECMA-262)";
 const searchNumber = str.match(/\d+/g);
 
 alert(`Знайдені числові значення: ${searchNumber}`);
+
+// Найти все символы в верхнем регистре ECMAScript 2015 (6th Edition, ECMA-262)”
+
+const searchUppercase = str.match(/[A-Z]/g);
+
+alert(`Знайдені великі літери: ${searchUppercase}`);
